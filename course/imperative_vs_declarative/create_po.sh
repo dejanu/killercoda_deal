9288b8dc33d09211615889a7b7f41a3e99f94dfb
dejanualex <dejanualexandru@gmail.com>
2024-03-16 00:31:34 +0200

    add scenario
    Signed-off-by: dejanualex <dejanualexandru@gmail.com>

--- course/imperative_vs_declarative/create_po.sh
+++
@@ -1,5 +0,0 @@
-# array
-pod_array=("app1" "app2")
-for i in "${pod_array[@]}"; do kubectl run "$i" --image=nginx:mainline-alpine3.18-slim --labels="env=preprod";done
-kubectl run app3 --image=nginx:mainline-alpine3.18-slim --labels="env=prod"
-kubectl run app0 --image=nginx:mainline-alpine3.18-slim --labels="env=dev"
