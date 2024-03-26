87ad9f845ad0d6c283b1be136fcd90700e0832f5
dejanualex <dejanualexandru@gmail.com>
2024-03-26 12:00:06 +0200

    add new question to quiz
    Signed-off-by: dejanualex <dejanualexandru@gmail.com>

--- course1/quiz/verify1.sh
+++ course1/quiz/verify1.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 # Run the command and count the number of lines
-count=$(grep "Systematic" /tmp/two | wc -l)
+count=$(grep -iE "cowsay|Hello World!" /tmp/two | wc -l)
 
+# Check if the count is not equal to 2
+if [ $count -ne 2 ]; then
+    exit 1
+fi
---
+++ course1/quiz/verify2.sh
@@ -0,0 +1,1 @@
+grep "Systematic" /tmp/two
