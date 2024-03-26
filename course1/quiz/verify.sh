80b52894cea3921604c0968725a49b5df72ca366
dejanualex <dejanualexandru@gmail.com>
2024-03-26 11:41:51 +0200

    docker course oci bundle
    Signed-off-by: dejanualex <dejanualexandru@gmail.com>

--- course1/quiz/verify.sh
+++ course1/quiz/verify.sh
@@ -1,2 +1,9 @@
-# check matches in 
-grep -iE "cowsay|Hello World!" /tmp/one | wc -l
+#!/bin/bash
+
+# Run the command and count the number of lines
+count=$(grep -iE "cowsay|Hello World!" /tmp/one | wc -l)
+
+# Check if the count is not equal to 2
+if [ $count -ne 2 ]; then
+    exit 1
+fi
