9a8e96fb827bb26a2281ad877baee51748e702f3
dejanualex <dejanualexandru@gmail.com>
2024-09-28 20:03:28 +0300

    break kubelet
    Signed-off-by: dejanualex <dejanualexandru@gmail.com>

--- course/k8s_architecture/breakkubelet.sh
+++ course/k8s_architecture/breakkubelet.sh
@@ -1,1 +1,5 @@
-ssh node01 'sed -i "s|/usr/bin/kubelet|/usr/local/bin/kubelet|g" /lib/systemd/system/kubelet.service.d/10-kubeadm.conf'
+# The unit file, source configuration file or drop-ins of kubelet.service changed on disk. 
+# Run 'systemctl daemon-reload' to reload units.
+
+sed -i 's|/usr/bin/kubelet|/usr/local/bin/kubelet|' /usr/lib/systemd/system/kubelet.service.d/10-kubeadm.conf
+sed -i 's|ExecStart=/usr/bin/kubelet|ExecStart=/usr/local/bin/kubelet|' /lib/systemd/system/kubelet.service
