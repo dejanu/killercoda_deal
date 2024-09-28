36796ab5e35b3346bc4342e6f3d4f7d383787810
dejanualex <dejanualexandru@gmail.com>
2024-09-28 20:07:32 +0300

    break kubelet
    Signed-off-by: dejanualex <dejanualexandru@gmail.com>

--- course/k8s_architecture/breakkubelet.sh
+++ course/k8s_architecture/breakkubelet.sh
@@ -3,3 +3,5 @@
 
 sed -i 's|/usr/bin/kubelet|/usr/local/bin/kubelet|' /usr/lib/systemd/system/kubelet.service.d/10-kubeadm.conf
 sed -i 's|ExecStart=/usr/bin/kubelet|ExecStart=/usr/local/bin/kubelet|' /lib/systemd/system/kubelet.service
+systemctl daemon-reload
+systemctl restart kubelet
