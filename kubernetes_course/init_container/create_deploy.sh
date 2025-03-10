e933579b5be349e87f362a68b880b2fb62138f30
dejanu <dejanualexandru@gmail.com>
2025-03-10 11:15:52 +0200

    update order

--- kubernetes_course/init_container/create_deploy.sh
+++ kubernetes_course/init_container/create_deploy.sh
@@ -4,4 +4,40 @@
 docker pull dejanualex/dockersay:2.1
 
 # pull deployment
-curl https://raw.githubusercontent.com/dejanu/killercoda_deal/main/course/init_container/deployment.yaml -o deployment.yaml
+# curl https://raw.githubusercontent.com/dejanu/killercoda_deal/main/course/init_container/deployment.yaml -o deployment.yaml
+
+# create deployment
+cat > deployment.yaml <<- "EOF"
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: nginx-deployment
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: nginx
+  template:
+    metadata:
+      labels:
+        app: nginx
+    spec:
+      restartPolicy: Always
+      volumes:
+        - name: data
+          emptyDir: {}
+      initContainers:
+        - name: nginx-init
+          image: docker/whalesay
+          command: [sh, -c]
+          args: [echo "<pre>$(cowsay -b 'Hello Kubernetes')</pre>" > /data/index.html]
+          volumeMounts:
+            - name: data
+              mountPath: /data
+      containers:
+        - name: nginx
+          image: nginx:1.11
+          volumeMounts:
+            - name: data
+              mountPath: /usr/share/nginx/html
+EOF
