19def9522239e087ccca000782b5a0e5e83af832
dejanu <dejanualexandru@gmail.com>
2025-10-10 16:40:11 +0300

    update creating pods
    Signed-off-by: dejanu <dejanualexandru@gmail.com>

--- kubernetes_course/k8s_scheduling_labels/create_po.sh
+++ kubernetes_course/k8s_scheduling_labels/create_po.sh
@@ -4,3 +4,4 @@ for i in "${pod_array[@]}"; do kubectl run "$i" --image=nginx:mainline-alpine3.1
 kubectl run app3 --image=nginx:mainline-alpine3.18-slim --labels="env=prod"
 kubectl run app0 --image=nginx:mainline-alpine3.18-slim --labels="env=dev"
 kubectl run app4 --image=nginx:mainline-alpine3.18-slim --labels="foo=try"
+
